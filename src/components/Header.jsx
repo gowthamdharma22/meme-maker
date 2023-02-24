f8647ea04e2a6791b6c5cebef6785f8dae49edd3
Gowtham Dharma <gowthamdharma15@gmail.com>
2023-02-24 19:05:03 +0530

    changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header(){
     return(
         <header className="header">
             <img src="img/TrollFace.png" className="header-img"/>
-            <h2 className="header-h2">Meme Generator</h2>
+            <h2 className="header-h2">Meme Maker</h2>
             <h4 className="header-h4">GD-Dev</h4>
         </header>
     )
